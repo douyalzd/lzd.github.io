9bddb3e4dafc24782966f82757fcea08d72ae170
luzhidong <873935709@qq.com>
2021-08-28 23:39:11 +0800

    增加打字效果

--- gulpfile.js
+++ gulpfile.js
@@ -73,11 +73,11 @@ gulp.task("compressJs", function() {
     .pipe(gulpif(!isScriptAll, changed("./public")))
     .pipe(gulpif(isDebug, debug({ title: "Compress JS:" })))
     .pipe(plumber())
-    .pipe(
-      gulpBabel({
-        presets: [es2015Preset] // es5检查机制
-      })
-    )
+    // .pipe(
+    //   gulpBabel({
+    //     presets: [es2015Preset] // es5检查机制
+    //   })
+    // )
     .pipe(uglify()) //调用压缩组件方法uglify(),对合并的文件进行压缩
     .pipe(gulp.dest("./public")); //输出到目标目录
 });
@@ -87,7 +87,7 @@ gulp.task("compressCss", function() {
   var option = {
     rebase: false,
     //advanced: true,               //类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
-    compatibility: "ie7" //保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
+    compatibility: "IE9+" //保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
     //keepBreaks: true,             //类型：Boolean 默认：false [是否保留换行]
     //keepSpecialComments: '*'      //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
   };
